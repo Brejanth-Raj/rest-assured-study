f87d43ba743ce4837dfab424050916d369060215
Brejanth Rajendran <brejanth.rajendran@cgi.com>
2022-02-02 14:17:48 -0500

    Added in testGetLegumes, tests for the name zucucchini

--- src/test/java/com/xyzcorp/LegumesFile.java
+++ src/test/java/com/xyzcorp/LegumesFile.java
@@ -8,29 +8,29 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class FruitFile{
+public class LegumesFile{
  //Rest-Assured  
  //[{"description":"Winter fruit","name":"Apple"},
  //{"description":"Tropical fruit","name":"Pineapple"}]
 
 
+
+
     @Test
-    public void testGetFruits() {
+    public void testGetLegumes() {
         given()
             .relaxedHTTPSValidation()
             .accept(ContentType.JSON)
             .when()
-            .get("https://staging.tiered-planet.net/mild-temper/fruits")
+            .get("https://staging.tiered-planet.net/mild-temper/legumes")
             .then()
             .assertThat()
-            .body("[0].description", equalTo("Winter fruit"))
+            .body("[1].name", equalTo("Zucchini"))
             
             
             
             ;
 
     }
-
-   
 
 }
